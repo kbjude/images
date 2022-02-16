e4f9b9740a2dff8220c5240fd9eed611c6b889b9
jkajura <jkajura@innovationvillage.co.ug>
2022-02-16 13:47:53 +0300

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :description)
+      params.require(:post).permit(:name, :description, :image)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-    has_many :images
+    has_one_attached :image
 end
