0943e49fa2e0f3d1780fa641c04744887d2eada4
jkajura <jkajura@innovationvillage.co.ug>
2022-02-17 14:15:51 +0300

    WIP devise authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :description, :image)
+      params.require(:post).permit(:name, :description, :image, avartars: [])
     end
 end
